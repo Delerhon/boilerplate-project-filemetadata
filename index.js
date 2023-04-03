ec6639e0a49a1d21af43f65ea3b2a407fabdab69
Delerhon <66553893+Delerhon@users.noreply.github.com>
2023-04-03 17:21:15 +0200

    packs added and multer configured

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const color = require('colors')
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage})
 
 var app = express();
+
+const errorHandler = (req, res, err) => {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        console.log('Multer Error'.bgRed.black)
+        res.send('Multer Error')
+        
+      } else if (err) {
+        // An unknown error occurred when uploading.
+        console.log('Unknown Error'.bgRed.black)
+        res.send('Unknown Error')
+      }
+  
+      // Everything went fine.
+    }
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +32,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse' , upload.single('upfile'), (req , res, next)=>{
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
